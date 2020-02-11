61a39ecdff9e8ef3954ff622b272d15a926a9151
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-02-10 23:00:49 -0500

    Juego con estructura HTML y CSS completas, ademas con la logica de JS para iluminar los colores y leer los clicks

---
+++ main.js
@@ -0,0 +1,78 @@
+const celeste = document.getElementById("celeste")
+const violeta = document.getElementById("violeta")
+const naranja = document.getElementById("naranja")
+const verde = document.getElementById("verde")
+const btnEmpezar = document.getElementById("btnEmpezar")
+
+class Juego {
+    constructor() {
+        this.inicializar()
+        this.generarSecuencia()
+        this.siguienteNivel()
+    }
+
+    inicializar() {
+        this.elegirColor = this.elegirColor.bind(this)
+        btnEmpezar.classList.add("hide")
+        this.level = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    siguienteNivel() {
+        this.iluminarSecuencia()
+        this.agregarEventosClick()
+    }
+
+    numeroAColor(num) {
+        switch (num) {
+            case 0:
+                return "celeste"
+            case 1:
+                return "violeta"
+            case 2:
+                return "naranja"
+            case 3:
+                return "verde"
+        }
+    }
+
+    iluminarSecuencia() {
+        for (let i = 0; i < this.level; i++) {
+            const color = this.numeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), i * 1000)
+        }
+    }
+
+    iluminarColor(color) {
+        this.colores[color].classList.add("light")
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+
+    apagarColor(color) {
+        this.colores[color].classList.remove("light")
+    }
+
+    agregarEventosClick() {
+        this.colores.celeste.addEventListener("click", this.elegirColor)
+        this.colores.violeta.addEventListener("click", this.elegirColor)
+        this.colores.naranja.addEventListener("click", this.elegirColor)
+        this.colores.verde.addEventListener("click", this.elegirColor)
+    }
+
+    elegirColor(ev) {
+        console.log(ev)
+    }
+}
+
+function empezarJuego() {
+    window.juego = new Juego()
+}
